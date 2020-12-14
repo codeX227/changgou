d2f08f5fd2f777ffb294a33fde487eadb21d76c4
xushuanglong <1872744675@qq.com>
2020-12-14 18:58:11 +0800

    build

---
+++ changgou-service/changgou-service-content/src/main/java/com/changgou/content/ContentApplication.java
@@ -0,0 +1,17 @@
+package com.changgou.content;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan(basePackages = {"com.changgou.content.dao"})
+public class ContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class);
+    }
+}
